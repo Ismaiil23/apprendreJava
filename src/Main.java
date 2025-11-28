65981cca052e706917c01025cd916256de9d1618
Ismaiil23 <mkisma84@gmail.com>
2025-11-28 14:46:39 +0100

    exercice app textuel, case 1 fait

--- src/Main.java
+++ src/Main.java
@@ -1,86 +1,112 @@
+import java.io.DataInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // Exo ArrayList
-//        ArrayList<CompteBancaire> listeComptes = new ArrayList<>();
-//        listeComptes.add(new CompteCourant("Alice",200,300));
-//        listeComptes.add(new CompteCourant("Bob",500,300));
-//        listeComptes.add(new CompteCourant("Charlie",1000,500));
-//
-//        for (CompteBancaire compte : listeComptes){
-//            compte.deposer(10);
-//        }
-//        for (CompteBancaire compte : listeComptes){
-//            compte.afficherSolde();
-//        }
-//
-        //Banque
+
+        var console = System.in;
+        Scanner scannerConsole= new Scanner(console);
+
         Banque banque = new Banque();
-        banque.ajouterCompte(new CompteCourant("Alice",200,300));
-        banque.ajouterCompte(new CompteCourant("Bob",500,300));
-        banque.ajouterCompte(new CompteCourant("Charlie",1000,500));
-        banque.afficherTout();
 
-        //Exo Heritage/Encapsulation
-//        CompteEpargne compteEp = new CompteEpargne("Picsou",1000,0.05);
-//        compteEp.afficherSolde();
-//        compteEp.appliquerInterets();
-//        compteEp.afficherSolde();
+        var enFonction= true;
 
-        //Exo Polymorphisme/Override
-        //listeComptes.add(compteEp);
+        while (enFonction){
+            afficherMenu();
+            int i = scannerConsole.nextInt();
+            scannerConsole.nextLine();
 
-//        for (CompteBancaire compte: listeComptes){
-//            compte.afficherSolde();
-//        }
+            switch (i){
+                case 1 :
+                    creationCompte(scannerConsole,banque);
+                    break;
+                case 2 :
+                    break;
 
-//        CompteCourant compteAlice = (CompteCourant) listeComptes.get(0);
-//        //compteAlice.retirer(300);
-//        compteAlice.afficherSolde();
-//        compteEp.afficherSolde();
-//        //compteAlice.retirer(300);
-//
-//        System.out.println("--- Tentative de Virement ---");
-//        compteAlice.effectuerVirement(compteEp, 50);
-//        compteAlice.afficherSolde();
-//        compteEp.afficherSolde();
+                case 3 :
+                    break;
 
-        //Virement plus propre
-        CompteBancaire compteAl = banque.rechercherCompte("1");
-        CompteBancaire compteBob = banque.rechercherCompte("2");
+                case 4 :
+                    break;
 
-        if(compteAl instanceof CompteCourant){
-            ((CompteCourant) compteAl).effectuerVirement(compteBob, 40);
+                case 5 :
+                    enFonction=false;
+                    break;
 
-        }else{
-            System.err.println("Opération impossible pour ce type de compte");
+            }
+
         }
-        compteAl.afficherSolde();
-        compteBob.afficherSolde();
-        //Virement encore mieux
-        banque.virer("2","1",100);
-        compteAl.afficherSolde();
-        compteBob.afficherSolde();
-        //Initiation Exceptions
-
-        banque.virer("1","2",10000);
-        compteAl.afficherSolde();
-
-        System.out.println("Ajout nouveau compte Alice");
-
-        banque.ajouterCompte(new CompteCourant("Alice",200,300));
-
-
-        List<CompteBancaire> comptesAlice = banque.rechercherComptesParNom("alice");
-
-        System.out.println("Afficher les comptes Alice");
-
-        banque.afficherComptesSelonListe(comptesAlice);
 
 
     }
+
+    private static void afficherMenu(){
+        System.out.println("\n=== MENU BANQUE ===");
+        System.out.println("1. Créer un compte");
+        System.out.println("2. Afficher un compte (par numéro)");
+        System.out.println("3. Créer un virement");
+        System.out.println("4. Rechercher des comptes par nom");
+        System.out.println("5. Quitter");
+        System.out.print("Votre choix : ");
+    }
+    public static void clear() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println();
+        }
+    }
+
+    public static void creationCompte(Scanner scannerConsole,Banque banque){
+        clear();
+        System.out.println("Quel est votre nom :");
+        String titu = scannerConsole.nextLine();
+        clear();
+        System.out.println("Quel est le montant de votre premier depot : ");
+        double solde = scannerConsole.nextDouble();
+        scannerConsole.nextLine();
+        clear();
+        System.out.println("1. Compte courant taper/ 2. Compte epargne");
+        System.out.println("Votre choix : ");
+        int i1 = scannerConsole.nextInt();
+        scannerConsole.nextLine();
+        clear();
+        if(i1==1){
+            System.out.println("Votre decouvert autorise sera de :\n1. 200/ 2. 400");
+            int decouvert = scannerConsole.nextInt();
+            if(decouvert==1){
+                decouvert=200;
+            } else if (decouvert==2) {
+                decouvert=400;
+            }else {
+                throw new RuntimeException("Chiffre non valide");
+            }
+            scannerConsole.nextLine();
+            banque.ajouterCompte(new CompteCourant(titu,solde,decouvert));
+            afficherUnCompte(banque, titu);
+
+        } else if (i1==2) {
+            System.out.println("Taux d'interet (ex: 0.05): ");
+            double taux = scannerConsole.nextDouble();
+            scannerConsole.nextLine();
+            banque.ajouterCompte(new CompteEpargne(titu,solde,taux));
+            afficherUnCompte(banque, titu);
+
+        }else{
+            throw new RuntimeException("Chiffre non valide");
+        }
+        System.out.println("Compte cree !");
+
+    }
+
+    private static void afficherUnCompte(Banque banque, String titu) {
+        System.out.println("Voici le recap de vos comptes :");
+        var compte = banque.rechercherComptesParNom(titu);
+        banque.afficherComptesSelonListe(compte);
+    }
+
 }
